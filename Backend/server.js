4c7bea504bd8710f996b5112c709287454000650
avinashgautam898 <gautamavi898@gmail.com>
2025-08-10 12:15:10 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -6,7 +6,7 @@ const notesRoute=require('./routes/note')
 app.use(cors());
 app.use(express.json())
 
-mongoose.connect("mongodb://127.0.0.1:27017/notesdata", {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(()=>{
